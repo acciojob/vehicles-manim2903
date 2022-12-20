ead8d5ef0b10b13ba886e9f8dc991fe348d18433
mani <mani>
2022-12-20 13:17:11 +0530

    changes done

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,7 +3,11 @@ package com.driver;
 public class Boat implements WaterVehicle {
     String name;
     int capacity;
-    Boat(){}
+    public Boat(){}
+    public Boat(String name,int capacity){
+        this.capacity=capacity;
+        this.name=name;
+    }
     public String getVehicleName(){
         return name;
     }
