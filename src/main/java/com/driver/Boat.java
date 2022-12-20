672fb68074ef9be56194dbaf65e160ea179c284e
mani <mani>
2022-12-20 13:02:16 +0530

    vehicle

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,12 @@
+package com.driver;
+
+public class Boat implements WaterVehicle {
+    String name;
+    int capacity;
+    public String getVehicleName(){
+        return name;
+    }
+    public int getVehicleCapacity(){
+        return capacity;
+    }
+}
